b9d46a5de3c5d58abc599fd3f5c20d047e673e22
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2024-08-26 08:54:51 -0300

    Add files via upload

--- pokemon/main.ts
+++ pokemon/main.ts
@@ -1,9 +1,9 @@
 class player{
-    private ataque:number;
-    private defesa:number;
-    private velocidade:number;
-    private vida:number;
-    private pontos:number;
+    private ataque:number;//esse e a ataque do player
+    private defesa:number;//essa e a defesa do player
+    private velocidade:number;//essa e a velocidade do player
+    private vida:number;//essa e a vida do player
+    private pontos:number;//a propriedade pontos e os pontos que serao distribuidos sobre os status de vida,defesa,velocidade e ataque
     public constructor(){
             this.ataque=0;
             this.defesa=0;
@@ -12,53 +12,53 @@ class player{
             this.pontos=10;
     }
     
-    public getpontos():number{
+    public getpontos():number{//essa funcao retorna o atributo pontos
         return this.pontos;
     }
-    public setpontos(value:number):void{
+    public setpontos(value:number):void{//essa funcao altera o valor do atributo pontos
         this.pontos+=value;
     }
     
-    public getdefesa():number{
+    public getdefesa():number{//essa funcao retorna o atributo defesa
         return this.defesa;
     }
-    public setdefesa(value:number):void{
+    public setdefesa(value:number):void{//essa funcao altera o valor do atributo defesa
         this.defesa+=value;
     }
     
-    public getvelocidade():number{
+    public getvelocidade():number{//essa funcao retorna o atributo velocidade
         return this.velocidade;
     }
-    public setvelocidade(value:number):void{
+    public setvelocidade(value:number):void{//essa funcao altera o valor do atributo velocidade
         this.velocidade+=value;
     }
 
-    public getvida():number{
+    public getvida():number{//essa funcao retorna o atributo vida
         return this.vida;
     }
-    public setvida(value:number):void{
+    public setvida(value:number):void{//essa funcao altera o valor do atributo vida
         this.vida+=value;
     }
 
-    public getataque():number{
+    public getataque():number{//essa funcao retorna o atributo ataque
         return this.ataque;
     }
-    public setataque(value:number):void{
+    public setataque(value:number):void{//essa funcao altera o valor do atributo ataque
         this.ataque+=value;
     }
     
-    public minustatus(){
-        this.setpontos(-1);
+    public minustatus(){//essa funcao executa a operacao -- no valor pontos 
+        this.pontos--
     }
-    public plustatus(){
-        this.setpontos(+1);
+    public plustatus(){//essa funcao executa a operacao ++ no valor pontos
+        this.pontos++
     }
 }
 
 var p1=new player()
 var p2=new player()
 
-function setstatusp1(status,valor){
+function setstatusp1(status,valor){//essa funcao serve para alterar os atributos da variavel p1
     if (status==='ataque'){
         p1.setataque(valor);
     }
@@ -69,11 +69,11 @@ function setstatusp1(status,valor){
         p1.setvelocidade(valor);
     }
     else if(status==='vida'){
-        p1.setvelocidade(valor);
+        p1.setvida(valor);
     }
 }
 
-function setstatusp2(status,valor){
+function setstatusp2(status,valor){//essa funcao serve para alterar os atributos da variavel p2
     if (status==='ataque'){
         p2.setataque(valor);
     }
@@ -84,11 +84,14 @@ function setstatusp2(status,valor){
         p2.setvelocidade(valor);
     }
     else if(status==='vida'){
-        p2.setvelocidade(valor);
+        p2.setvida(valor);
     }
 }
 
-function setstatusp(valor:number,status:string,p:number){
+function setstatusp(valor:number,status:string,p:number){//essa funcao serve para generalizar as funcoe setstatusp2 e setstatusp1
+    //sendo o atributo p, que aparecera bastante nas proximas funcoes, o player a qual estou se referindo
+    //o valor e o valor a qual vou somar ao status desejado
+    // e o status se refere a qual status quero alteras
     if(p===1){
         setstatusp1(status,valor)
     }
@@ -97,7 +100,7 @@ function setstatusp(valor:number,status:string,p:number){
     }
 
 }
-function setpontosp1(){
+function setpontosp1(){//essa funcao serve para alterar o atributos pontos no objeto p1, e atualizar no html
     if (p1.getpontos() > 1) {
         p1.minustatus()
         document.getElementById('pontos1').innerText = p1.getpontos().toString(); // Atualiza a exibição dos pontos restantes
@@ -106,7 +109,7 @@ function setpontosp1(){
         document.getElementById('statusp1').innerHTML = '';
     }
 }
-function setpontosp2(){
+function setpontosp2(){//essa funcao serve para alterar o atributos pontos no objeto p2, e atualizar no html
     if (p2.getpontos() > 1) {
         p2.minustatus()
         document.getElementById('pontos2').innerText = p2.getpontos().toString(); // Atualiza a exibição dos pontos restantes
@@ -116,6 +119,8 @@ function setpontosp2(){
     }
 }
 function setpontos(p:number){
+    //essa funcao serve para geralizar as funcoes setpontosp2 e setpontosp1
+    //a funcao p assume 1 ou 2 se referindo a qual player quer se referir
     if(p===1){
         setpontosp1()
     }
@@ -127,4 +132,4 @@ function setpontos(p:number){
 function setp(valor:number, id:string, status:string,p:number) {
     setstatusp(valor,status,p)
     setpontos(p)
-}
+}//essa e a funcao geral de tudo alterando o atributo status, atualizando o html, e atualizando o atributo pontos
